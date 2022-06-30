7e6e4a9484ae1a192206ac9351fad089dc521d56
onur <kizilayonurtahsin@gmaik.com>
2022-06-30 17:13:56 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+      <footer className="page-footer">
+        <div className="container">
+          <small>
+            Onur Tahsin Kızılay and FS Team, Teacher Felix
+          </small>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+      <section className="top-banner">
+        <div className="container">
+          <center>
+            <img
+              //! src="https://d1psgljc389n8q.cloudfront.net/bootcamps/logos/VX6tpIUJa" 
+              //? clarusway project olarak yaptım teşekkürler clarusway
+              src="https://cdn-icons-png.flaticon.com/512/240/240605.png"
+              alt="Bank logo"
+              className="nav__logo"
+              id="logo"
+            />
+            <h1 className="heading">Weather App</h1>
+          </center>
+        </div>
+      </section>
+    );
+  };
+  export default Header;
